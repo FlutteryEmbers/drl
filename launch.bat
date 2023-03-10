5afc10271f33d1a6744bbf7fbe01bfb2caeea74c
shili.wu <shili.wu@hotmail.com>
2023-03-10 05:41:41 -0600

    10_10_config

--- launch.bat
+++ launch.bat
@@ -1,2 +1,4 @@
-python algo_envs_v2/ppo_microrts_share_gae.py --f microrts_16_16_loss0 --cuda 0
-python algo_envs_v2/ppo_microrts_share_gae.py --f microrts_16_16_loss0 --cuda 0
+python algo_envs_v2/ppo_microrts_share_gae.py --f microrts_10_10_loss0 --cuda 0
+python algo_envs_v2/ppo_microrts_share_gae.py --f microrts_10_10_loss0 --cuda 0
+python algo_envs_v2/ppo_microrts_share_pae_condition.py --f microrts_10_10_loss0 --cuda 0
+python algo_envs_v2/ppo_microrts_share_pae_condition.py --f microrts_10_10_loss0 --cuda 0
